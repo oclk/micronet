c07e3826d30654a61f32d15633c082ad697f9aca
Ömer Çelik <oclk@outlook.com.tr>
2023-11-11 01:13:16 +0300

    feat: Create ContentTypes.cs and rebuild BuildingBlocks structure

---
+++ Source/BuildingBlocks/Shared/Constants/ContentTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Constants;
+
+public class ContentTypes
+{
+    public const string Json = "application/json";
+    public const string Xml = "application/xml";
+    public const string FormUrlEncoded = "application/x-www-form-urlencoded";
+    public const string MultipartFormData = "multipart/form-data";
+}
