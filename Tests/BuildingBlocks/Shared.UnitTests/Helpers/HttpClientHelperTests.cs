3af1e54c603d00d8ffb190f69848bdc5453538fb
Ömer Çelik <oclk@outlook.com.tr>
2023-11-11 04:18:57 +0300

    feat: Added SendRESTRequestAsync_FormUrlEncodedContentType_SuccessfulRequest_ReturnsDeserializedObject the the Shared.UnitTests

--- Tests/BuildingBlocks/Shared.UnitTests/Helpers/HttpClientHelperTests.cs
+++ Tests/BuildingBlocks/Shared.UnitTests/Helpers/HttpClientHelperTests.cs
@@ -71,6 +71,38 @@ public class HttpClientHelperTests
         Assert.Equal(expectedResponse.Property1, result.Property1);
         Assert.Equal(expectedResponse.Property2, result.Property2);
     }
+
+    [Fact]
+    public async Task SendRESTRequestAsync_FormUrlEncodedContentType_SuccessfulRequest_ReturnsDeserializedObject()
+    {
+        // Arrange
+        var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+        var httpClientMock = new Mock<HttpClient>();
+
+        var expectedResponse = new TestResponse { Property1 = "Value1", Property2 = 42 };
+        var jsonResponse = JsonConvert.SerializeObject(expectedResponse);
+
+        var responseMessage = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent(jsonResponse, System.Text.Encoding.UTF8, ContentTypes.APPLICATION_JSON)
+        };
+
+        httpClientMock.Setup(x => x.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(responseMessage);
+
+        httpClientFactoryMock.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(httpClientMock.Object);
+
+        var httpClientHelper = new HttpClientHelper(httpClientFactoryMock.Object, TimeSpan.FromSeconds(1), 2, 3);
+
+        // Act
+        var result = await httpClientHelper.SendRESTRequestAsync<TestRequest, TestResponse>(
+            HttpMethod.Get, "http://example.com", new TestRequest(), ContentTypes.APPLICATION_X_WWW_FORM_URLENCODED, null, null);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedResponse.Property1, result.Property1);
+        Assert.Equal(expectedResponse.Property2, result.Property2);
+    }
 }
 
 #region Sample Request and Response classes for testing
