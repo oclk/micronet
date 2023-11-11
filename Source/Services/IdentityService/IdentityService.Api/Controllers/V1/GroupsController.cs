b054f205e55c51d751e350bbe69ae8b9fec9792e
Ömer Çelik <oclk@outlook.com.tr>
2023-11-11 13:49:18 +0300

    feat: Created all endpoints of Groups

--- Source/Services/IdentityService/IdentityService.Api/Controllers/V1/GroupsController.cs
+++ Source/Services/IdentityService/IdentityService.Api/Controllers/V1/GroupsController.cs
@@ -4,13 +4,67 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityService.Api.Controllers.V1;
 
-[Route("IdentityService/Api/V{version:apiVersion}/[controller]")]
+[Route("IdentityService/Api/V{version:apiVersion}/{realm}/[controller]")]
 [ApiVersion("1.0")]
 [Authorize]
 public class GroupsController : BaseController
 {
+    [HttpGet("Count")]
+    public IActionResult GetGroupsCount(string realm)
+    {
+        return Ok();
+    }
+
     [HttpGet]
-    public IActionResult Index()
+    public IActionResult GetGroups(string realm)
+    {
+        return Ok();
+    }
+
+    [HttpPost("{id}/Children")]
+    public IActionResult SetOrCreateGroup(string realm, string id)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteGroup(string realm, string id)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetGroup(string realm, string id)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}/Management/Permissions")]
+    public IActionResult GetGroupManagementPermissions(string realm, string id)
+    {
+        return Ok();
+    }
+
+    [HttpPut("{id}/Management/Permissions")]
+    public IActionResult UpdateGroupManagementPermissions(string realm, string id)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}/Members")]
+    public IActionResult GetGroupMembers(string realm, string id)
+    {
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateGroup(string realm, string id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult CreateGroup(string realm, string id)
     {
         return Ok();
     }
