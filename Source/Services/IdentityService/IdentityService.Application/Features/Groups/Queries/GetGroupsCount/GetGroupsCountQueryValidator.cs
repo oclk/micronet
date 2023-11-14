f3ad31e294828303849813b993c09806656254ee
Ömer Çelik <oclk@outlook.com.tr>
2023-11-14 16:39:43 +0300

    feat: Created GetGroupsCountQueryValidator

---
+++ Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroupsCount/GetGroupsCountQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace IdentityService.Application.Features.Groups.Queries.GetGroupsCount;
+
+public class GetGroupsCountQueryValidator : AbstractValidator<GetGroupsCountQuery>
+{
+    public GetGroupsCountQueryValidator()
+    {
+        RuleFor(x => x.Realm)
+            .NotEmpty().WithMessage("Realm is required.");
+    }
+}
