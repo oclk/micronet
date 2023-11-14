6f88db09be376e617de5e2be2fb848c12a564464
Ömer Çelik <oclk@outlook.com.tr>
2023-11-14 16:42:25 +0300

    feat: Created GetGroupsQueryValidator

---
+++ Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroups/GetGroupsQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace IdentityService.Application.Features.Groups.Queries.GetGroups;
+
+public class GetGroupsQueryValidator : AbstractValidator<GetGroupsQuery>
+{
+    public GetGroupsQueryValidator()
+    {
+        RuleFor(x => x.Realm)
+            .NotEmpty().WithMessage("Realm is required.");
+    }
+}
