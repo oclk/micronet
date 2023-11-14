7a7a5f1bba67f928d48ebf820bda7dd8ffaee8e2
Ömer Çelik <oclk@outlook.com.tr>
2023-11-14 16:45:34 +0300

    refactor: Added some summaries to the GetGroupsQueryValidator and GetGroupsCounQueryValidator

--- Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroups/GetGroupsQueryValidator.cs
+++ Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroups/GetGroupsQueryValidator.cs
@@ -2,11 +2,20 @@
 
 namespace IdentityService.Application.Features.Groups.Queries.GetGroups;
 
+/// <summary>
+/// Validator class for validating the parameters of the GetGroupsQuery.
+/// </summary>
 public class GetGroupsQueryValidator : AbstractValidator<GetGroupsQuery>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetGroupsQueryValidator"/> class.
+    /// </summary>
     public GetGroupsQueryValidator()
     {
+        // Rule for validating that the Realm property is not empty.
         RuleFor(x => x.Realm)
             .NotEmpty().WithMessage("Realm is required.");
+
+        // You can add more rules here for other properties of GetGroupsQuery if needed.
     }
 }
--- Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroupsCount/GetGroupsCountQueryValidator.cs
+++ Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroupsCount/GetGroupsCountQueryValidator.cs
@@ -2,11 +2,20 @@
 
 namespace IdentityService.Application.Features.Groups.Queries.GetGroupsCount;
 
+/// <summary>
+/// Validator class for validating the parameters of the GetGroupsCountQuery.
+/// </summary>
 public class GetGroupsCountQueryValidator : AbstractValidator<GetGroupsCountQuery>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetGroupsCountQueryValidator"/> class.
+    /// </summary>
     public GetGroupsCountQueryValidator()
     {
+        // Rule for validating that the Realm property is not empty.
         RuleFor(x => x.Realm)
             .NotEmpty().WithMessage("Realm is required.");
+
+        // You can add more rules here for other properties of GetGroupsCountQuery if needed.
     }
 }
