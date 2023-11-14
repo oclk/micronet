57740b60fd6df3187635110b049a54b861980f61
Ömer Çelik <oclk@outlook.com.tr>
2023-11-14 16:35:40 +0300

    refactor: Update GetGroupsCountQuery class structure

---
+++ Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroupsCount/GetGroupsCountQueryParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace IdentityService.Application.Features.Groups.Queries.GetGroupsCount;
+
+/// <summary>
+/// Represents query parameters used to filter the group count.
+/// </summary>
+public class GetGroupsCountQueryParameters
+{
+    /// <summary>
+    /// Gets or sets the property for searching groups.
+    /// </summary>
+    public string Search { get; set; }
+
+    /// <summary>
+    /// Gets or sets a boolean property indicating whether to fetch a specific number of top groups.
+    /// </summary>
+    public bool Top { get; set; }
+}
---
+++ Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroupsCount/GetGroupsCountQueryResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityService.Application.Features.Groups.Queries.GetGroupsCount;
+
+/// <summary>
+/// Represents the response object containing the count of groups.
+/// </summary>
+public class GetGroupsCountQueryResponse
+{
+    /// <summary>
+    /// Gets or sets the total count of groups.
+    /// </summary>
+    public long Count { get; set; }
+}
