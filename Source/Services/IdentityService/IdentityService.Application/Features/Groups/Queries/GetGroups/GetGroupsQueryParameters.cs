3793d844409d7a71b44a91de505d21e0922e1a67
Ömer Çelik <oclk@outlook.com.tr>
2023-11-14 16:32:46 +0300

    refactor: Update GetGroupsQuery class structure

---
+++ Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroups/GetGroupsQueryParameters.cs
@@ -0,0 +1,42 @@
+﻿namespace IdentityService.Application.Features.Groups.Queries.GetGroups;
+
+/// <summary>
+/// Represents query parameters used to filter the group details.
+/// </summary>
+public class GetGroupsQueryParameters
+{
+    /// <summary>
+    /// Gets or sets a boolean property indicating whether to use brief representation.
+    /// </summary>
+    public bool BriefRepresentation { get; set; }
+
+    /// <summary>
+    /// Gets or sets a boolean property indicating whether to perform an exact match.
+    /// </summary>
+    public bool Exact { get; set; }
+
+    /// <summary>
+    /// Gets or sets the first result to retrieve.
+    /// </summary>
+    public string First { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum number of results to retrieve.
+    /// </summary>
+    public string Max { get; set; }
+
+    /// <summary>
+    /// Gets or sets a boolean property indicating whether to populate the hierarchy.
+    /// </summary>
+    public bool PopulateHierarchy { get; set; }
+
+    /// <summary>
+    /// Gets or sets the search query.
+    /// </summary>
+    public string Q { get; set; }
+
+    /// <summary>
+    /// Gets or sets the search string.
+    /// </summary>
+    public string Search { get; set; }
+}
---
+++ Source/Services/IdentityService/IdentityService.Application/Features/Groups/Queries/GetGroups/GetGroupsQueryResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace IdentityService.Application.Features.Groups.Queries.GetGroups;
+
+/// <summary>
+/// Represents the response object containing group details.
+/// </summary>
+public class GetGroupsQueryResponse
+{
+    /// <summary>
+    /// Gets or sets the identifier of the group.
+    /// </summary>
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the group.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the path of the group.
+    /// </summary>
+    public string Path { get; set; }
+
+    /// <summary>
+    /// Gets or sets subgroups of the current group.
+    /// </summary>
+    public List<GetGroupsQueryResponse> SubGroups { get; set; }
+}
